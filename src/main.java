270fea1f85f57f74266707173331ddf721552248
Connykrautstingl <s44784@edu.campus02.at>
2021-10-09 10:04:40 +0200

    2nd upload test

--- src/main.java
+++ src/main.java
@@ -3,5 +3,6 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("Ich bin ein Test");
+        System.out.println("Ich bin Zeile 2 zum 2ten Testupload");
     }
 }
